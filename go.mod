4308fcbced6d3cc23c67c6cafafa6c8fe0d8f364
yuanzilin <1799926597@qq.com>
2020-09-15 17:07:35 +0800

    解析了xlsx文件

--- go.mod
+++ go.mod
@@ -3,9 +3,11 @@ module awesomeProject
 go 1.14
 
 require (
+	github.com/360EntSecGroup-Skylar/excelize v1.4.1 // indirect
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/gin-gonic/gin v1.6.3
 	github.com/newrelic/go-agent v3.8.1+incompatible
+	github.com/tealeg/xlsx v1.0.5
 	go.mongodb.org/mongo-driver v1.3.5
 	golang.org/x/tools v0.0.0-20190531172133-b3315ee88b7d
 	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
--- main/util/file.go
+++ main/util/file.go
@@ -1,7 +1,9 @@
 package util
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/tealeg/xlsx"
 	"io"
 	"log"
 	"mime/multipart"
@@ -32,30 +34,9 @@ func Write(file *multipart.FileHeader,path string) bool{
 	}
 	return true
 }
-//func Write1(filestream multipart.File,path string) bool{
-//
-//
-//	defer filestream.Close()
-//	//if err!=nil{
-//	//	log.Println(err)
-//	//	return false
-//	//}
-//	os.Mkdir(path,os.ModePerm)
-//	out,err :=os.Create(path+"/"+"test")
-//	defer out.Close()
-//	if err!=nil{
-//		log.Println(err)
-//		return false
-//	}
-//	_,err=io.Copy(out,filestream)
-//	if err!=nil{
-//		log.Println(err)
-//		return false
-//	}
-//	return true
-//}
+
 func Read(path string)*os.File{
-	var file *os.File
+	//var file *os.File
 	log.Println(path)
 	file ,err:=os.Open(path)
 	if err!=nil{
@@ -81,5 +62,42 @@ func ReadLog(c *gin.Context){
 	}
 	http.ServeContent(c.Writer, c.Request, file.Name(), time.Now(), file)
 
+
+}
+
+//解析一个学号+姓名的表
+func AnalyzeExcel(path string) [1000][2]string{
+	xlFile, err:= xlsx.OpenFile(path)
+	if(err!=nil){
+		log.Println(err)
+	}
+
+
+	var result [1000][2] string
+
+
+	for _,sheet := range xlFile.Sheets {
+		for rowIndex, row := range sheet.Rows {
+			if (rowIndex > 0) {
+				if len(row.Cells) <2{
+					break
+				}
+				for cellIndex, cell := range row.Cells {
+					text := cell.String()
+					if(len(text)==0){
+						break
+					}
+					result[rowIndex-1][cellIndex] = text
+				}
+			}
+		}
+		break
+	}
+
+	for i:=0;i<3;i++{
+		fmt.Println(result[i][0]+result[i][1])
+	}
+
+	return result
 
 }
